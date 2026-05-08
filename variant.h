83dc401bf5e1f3c1f4f34151d0e1f5d3d939654a
Muhammad <thevancedgamer@mentallysanemainliners.org>
2026-05-08 14:21:43 +0500

    variant: add support for owned variants
    * useful for objects that own a reference, so it can get freed by the GC
    
    Signed-off-by: Muhammad <thevancedgamer@mentallysanemainliners.org>

--- variant.h
+++ variant.h
@@ -1,6 +1,9 @@
 #ifndef VARIANT_H
 #define VARIANT_H
 
+#include <stdbool.h>
+#include <stdlib.h>
+
 typedef struct Class Class;
 typedef struct Object Object;
 typedef struct Variant Variant;
@@ -15,6 +18,8 @@ typedef enum {
 
 typedef struct Variant {
     VariantType type;
+    bool owned;
+    int refcnt;
     union {
         Class *class;
         Object *object;
@@ -22,5 +27,58 @@ typedef struct Variant {
         int int_val;
     } data;
 } Variant;
+
+static inline void variant_acquire(Variant *variant)
+{
+    if (variant->type == VARIANT_TYPE_REF && variant->owned) {
+        variant->refcnt++;
+    }
+}
+
+static inline Variant variant_make_int(int value)
+{
+    Variant ret = (Variant){ .type = VARIANT_TYPE_INT, .data.int_val = value, .refcnt = 0 };
+    variant_acquire(&ret);
+    return ret;
+}
+
+static inline Variant variant_make_class(Class *class)
+{
+    Variant ret = (Variant){ .type = VARIANT_TYPE_CLASS, .data.class = class, .refcnt = 0 };
+    variant_acquire(&ret);
+    return ret;
+}
+
+static inline Variant variant_make_object(Object *object)
+{
+    Variant ret = (Variant){ .type = VARIANT_TYPE_OBJECT, .data.object = object, .refcnt = 0 };
+    variant_acquire(&ret);
+    return ret;
+}
+
+static inline Variant variant_make_ref(void *ref)
+{
+    Variant ret = (Variant){ .type = VARIANT_TYPE_REF, .data.ref = ref, .owned = false, .refcnt = 0 };
+    variant_acquire(&ret);
+    return ret;
+}
+
+static inline Variant variant_make_owned_ref(void *ref)
+{
+    Variant ret = (Variant){ .type = VARIANT_TYPE_REF, .owned = true, .data.ref = ref, .refcnt = 0 };
+    variant_acquire(&ret);
+    return ret;
+}
+
+static inline void variant_release(Variant *variant)
+{
+    if (!variant)
+        return;
+
+    if (variant->type == VARIANT_TYPE_REF && variant->owned && variant->data.ref && --variant->refcnt == 0) {
+        free(variant->data.ref);
+        variant->data.ref = NULL;
+    }
+}
 
 #endif
